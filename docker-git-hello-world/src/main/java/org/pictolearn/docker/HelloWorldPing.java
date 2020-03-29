3a2b299b742fe3778b8497fa4cd0f60cf4e8adbb
minthurein <minthuraine95@gmail.com>
2020-03-29 14:24:58 +0630

    latest

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,6 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
+			System.out.println("Hello World Thu Rein Ping " + i );
 			System.out.println("Hello World Ping " + i );
 			Thread.sleep(1000);
 		}
